2cdc4bca875a1dccc74372ea07ba71a0e787272d
amanag1498 <109754888+amanag1498@users.noreply.github.com>
2026-03-22 22:03:58 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Add:", add(10, 5))
+    print("Subtract:", subtract(10, 5))
